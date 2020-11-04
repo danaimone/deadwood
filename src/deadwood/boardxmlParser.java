782a1ef62d12d368ce567190a0e75780e96e3838
Keegan-Mangahas <55812247+Keegan-Mangahas@users.noreply.github.com>
2020-11-03 18:24:24 -0800

    Finished boardXML parser
    The boardxmlParser.java file gathers all data needed to create the board. It currently prints out the board data, but could easily make objects instead
    
    Running the Gamemaster main method will print all board data

--- src/deadwood/boardxmlParser.java
+++ src/deadwood/boardxmlParser.java
@@ -38,38 +38,78 @@ public class boardxmlParser{
             //get the rest of the elements in the current set
             NodeList setChildren = set.getChildNodes();
             for(int j = 0; j < setChildren.getLength(); j++){
+                Node sub = setChildren.item(j);
 
-                /*
-                current the rest of this only prints out each sets
-                neighbors, can probably change to print out #ofTakes
-                and 'parts' data
-                */
-                if(setChildren.item(j).getNodeType() == Node.ELEMENT_NODE){
+                //gets neighbor data
+                if("neighbors".equals(sub.getNodeName())){
                     Element el = (Element) setChildren.item(j);
                     NodeList neighbors = el.getElementsByTagName("neighbor");
-                    NodeList takes = el.getElementsByTagName("takes");
-                    NodeList parts = el.getElementsByTagName("part");
-
                     for(int x = 0; x < neighbors.getLength(); x++){
                         Node neighbor = neighbors.item(x);
                         String neighborName = neighbor.getAttributes().getNamedItem("name").getNodeValue();
 
                         System.out.println("    Neighbor: " + neighborName);
                     }
+                }
+                //gets set area data
+                else if("area".equals(sub.getNodeName())){
+                    String sX = sub.getAttributes().getNamedItem("x").getNodeValue();
+                    String sY = sub.getAttributes().getNamedItem("y").getNodeValue();
+                    String sH = sub.getAttributes().getNamedItem("w").getNodeValue();
+                    String sW = sub.getAttributes().getNamedItem("h").getNodeValue();
+                    System.out.printf("    Set area; x: %s, y: %s, h: %s, w: %s%n", sX, sY, sH, sW);
+                }
+                //gets takes/shotcounter data
+                else if("takes".equals(sub.getNodeName())){
+                    Element el = (Element) setChildren.item(j);
+                    NodeList takes = el.getElementsByTagName("take");
+                    int totalTakes = takes.getLength();
+                    System.out.println("    Set Takes: "+ totalTakes);
 
-                    for(int x = 0; x < parts.getLength(); x++){
-                        Node part = parts.item(x);
+                    //gets take data for later (gui stuff)
+                    for(int x = 0; x < takes.getLength(); x++){
+                        Node take = takes.item(x);
+                        String takeNumber = take.getAttributes().getNamedItem("number").getNodeValue();
+
+                        Element takeEl = (Element) takes.item(x);
+                        NodeList takeData = takeEl.getElementsByTagName("area");
+                        for(int y = 0; y < takeData.getLength(); y++){
+                            Node takeArea = takeData.item(y);
+                            String tX = takeArea.getAttributes().getNamedItem("x").getNodeValue();
+                            String tY = takeArea.getAttributes().getNamedItem("y").getNodeValue();
+                            String tH = takeArea.getAttributes().getNamedItem("h").getNodeValue();
+                            String tW = takeArea.getAttributes().getNamedItem("w").getNodeValue();
+                            System.out.printf("        Take area %s; x: %s, y: %s, h: %s, w: %s%n", takeNumber, tX, tY, tH, tW);
+                        }
+                    }
+                }
+                //gets set parts data
+                else if("parts".equals(sub.getNodeName())){
+                    Element el = (Element) setChildren.item(j);
+                    NodeList parts = el.getElementsByTagName("part");
+
+                    for(int y = 0; y < parts.getLength(); y++){
+                        Node part = parts.item(y);
                         String partName = part.getAttributes().getNamedItem("name").getNodeValue();
                         String difficulty = part.getAttributes().getNamedItem("level").getNodeValue();
-                        System.out.printf("        Part: %s, Difficulty: %s%n", partName, difficulty);
-                        
-                    }
+                        System.out.printf("    Part: %s, Difficulty: %s%n", partName, difficulty);
 
-
-                    /*
-                    if(el.getNodeName().contains("neighbor")){
+                        NodeList partData = part.getChildNodes();
+                        for(int z = 0; z < partData.getLength(); z++){
+                            Node partDataNode = partData.item(z);
+                            if("area".equals(partDataNode.getNodeName())){
+                                String pX = partDataNode.getAttributes().getNamedItem("x").getNodeValue();
+                                String pY = partDataNode.getAttributes().getNamedItem("y").getNodeValue();
+                                String pH = partDataNode.getAttributes().getNamedItem("w").getNodeValue();
+                                String pW = partDataNode.getAttributes().getNamedItem("h").getNodeValue();
+                                System.out.printf("        Part area; x: %s, y: %s, h: %s, w: %s%n", pX, pY, pH, pW);
+                            }
+                            if("line".equals(partDataNode.getNodeName())){
+                                String line = partDataNode.getTextContent();
+                                System.out.printf("        Line: %s%n", line);
+                            }
+                        }
                     }
-                    */
                 }
             }
         }
