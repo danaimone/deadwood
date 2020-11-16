26788c5516ef7e3fdee3cd3b48af2e5bfc51c965
Dan Aimone <30274286+danaimone@users.noreply.github.com>
2020-11-16 01:23:00 -0800

    updated Gamemaster constructor to include a printer passing constructor

--- src/deadwood/Gamemaster.java
+++ src/deadwood/Gamemaster.java
@@ -4,58 +4,72 @@ import java.util.*;
 
 public class Gamemaster {
     private Board board;
+    private DeadwoodPrinter printer;
 
     private PlayerController playerController;
-    private PlayerInput playerInput = new PlayerInput();
 
     // TODO: arguable, sceneCards
     // need some sort of Scene controller
     private ArrayList<Scene> sceneCards = new ArrayList<Scene>(); //stores all the scene cards and their data
 
-    /* Constructor Singleton */
+    /**
+     * In the case that a printer is not passed, a new one will be created instead.
+     */
     public Gamemaster() {
-        board = new Board();
+        this.printer = new DeadwoodPrinter();
+    }
 
+    /**
+     * Constructor for Gamemaster, in the case that you would like to pass a printer.
+     * @param printer the DeadwoodPrinter to be used among the package
+     */
+    public Gamemaster(DeadwoodPrinter printer) {
+        board = new Board();
+        playerController = new PlayerController();
+        this.printer = printer;
     }
 
     /**
      * Play Deadwood
-     *
+     * <p>
      * This is the driving force behind the entire program.
      * Invoking this method will run the console program of Deadwood.
      * Make sure this is what you want to do!
-     *
+     * <p>
      * This is package protected, since no one should need access to this outside of the package.
+     *
      * @return The winner of this entire game!
      */
     String playDeadwood() {
         System.out.println("Welcome to Deadwood!");
         setupPlayers();
-        // TODO: Here's where we set up players.
-        //       This involves grabbing their names,
-        //       and getting all the related number specific jazz setup!
+        while (board.boardData.getDaysLeft() > 0) {
+            // TODO: - display locations
+            DeadwoodPr
+
+        }
 
         return "It was me all along!";
     }
 
     /**
      * Setup Players
-     *
+     * <p>
      * Gets all player names and player inputs
      * This takes care of setting up all the player objects
      * and the players for the Board in its entirety, since
      * those are inherently built into their respective classes.
-     *
+     * <p>
      * This function also sets up the board a bit more.
      * The board setting up could be separated specifically into
      * class related functions, so this function could use some work.
-     *
      */
     private void setupPlayers() {
-        int numberOfPlayers = playerInput.getNumberOfPlayers();
+        int numberOfPlayers = playerController.playerInput.getNumberOfPlayers();
         board.boardData = new BoardData(numberOfPlayers);
         board.boardData.setDaysLeft(numberOfPlayers);
         board.addPlayersToBoard(numberOfPlayers);
+        board.boardData.setInitialCurrentPlayer();
     }
 
     private int calculateScore(PlayerController playerController) {
